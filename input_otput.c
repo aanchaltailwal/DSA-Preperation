61c467c0137604324bd6216dbf949e6c7a5e6f35
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-09 09:56:48 +0530

    input_output

---
+++ input_otput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    char b;
+    /*float c;
+    double d;*/
+    scanf("%d", &a);
+    scanf("%c" , &b);
+    printf("%d\n", a);
+    printf("%c\n", b);
+
+    return 0;
+}
