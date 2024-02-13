36472adb31a4045534476bd2d4e82d0a73ad6873
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-13 08:27:56 +0530

    prime no

---
+++ prime_no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+
+    for(a=0; a<=20; a++);
+
+    if(a / a ==0 || a/2 == 0){
+        printf("The prime no");
+
+    } 
+    else{
+        printf("not a prime no");
+    }
+    return 0;
+}
