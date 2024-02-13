7eac4bf7ed28a6f52db8b1a7f7606b47920e8723
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-13 22:00:05 +0530

    prime numbers

--- prime_no.c
+++ prime_no.c
@@ -1,18 +1,28 @@
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d", &n);
-    
-    for (int i=2; i>=n; i++){      
+#include <stdio.h>
+int main() {
+    int i, j, is_prime;
 
-        int div = 1;
-        for (int j=2; j*j<=i; j++){
-            if (i%j == 0) div++;
+    printf("Prime numbers till 100 are :\n");
 
+    // Loop through numbers from 2 to 100
+    for (i = 2; i <= 100; ++i) {
+        // Assume the number is prime
+        is_prime = 1;
+
+        // Check if the number is divisible by any number other than 1 and itself
+        for (j = 2; j <= i / 2; ++j) {
+            if (i % j == 0) {
+                is_prime = 0;
+                break;
+            }
         }
-        if(div==1) printf("%d\t", i);
 
-        
+        // If the number is prime, print it
+        if (is_prime)
+            printf("%d ", i);
+    }
+
+    printf("\n");
 
     return 0;
 }
