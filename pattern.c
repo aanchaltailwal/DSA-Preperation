7aadbc12214c4cbb1cba7433b4c3d6f9105fd1e8
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-14 13:24:11 +0530

    123 pattern

---
+++ pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    for (int rn = 1; rn<=n; rn ++){
+        //spaces------> 2zzzzzz*(n-rn)
+        for (int sp = 1; sp<=2*(n-rn); sp++) printf(" ");
+        //nums
+        for(int num =1; num<=rn; num++) printf("%d", num);
+        if(rn != 1){
+            for(int sp = 1; sp<=2*rn-3; sp++) printf(" ");
+            for(int num = 1; num<=rn; num++) printf("%d",  num);
+        }
+    printf("\n");
+}
+return 0;
+}
