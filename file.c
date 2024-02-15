e1854c9db6a369f828eb460165010542f541a3d7
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-15 15:30:23 +0530

    array

--- file.c
+++ file.c
@@ -2,7 +2,9 @@
 
 int main() {
     
-    printf("Hello, World!\n");
-    return 0;
+    int arr[][3]={{1,2,3}, {4,5,6},{7,8,9}};
+    printf("%d\t%d", arr[1][1], arr[0][4]);
+    printf("\n%p\t%p", arr, arr + 1);
+    
+    
 }
-/* [9,7,3,1,10,2,5,4,8,6] */
